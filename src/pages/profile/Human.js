036f30e02231b2a0168f5a3c8aae9e4426588e42
Cyril PARODI <parodicyril@gmail.com>
2019-08-25 11:35:49 +0200

    petites animations

--- src/pages/profile/Human.js
+++ src/pages/profile/Human.js
@@ -1,43 +1,60 @@
-import React from "react"
+import React from "react";
+import { Spring } from "react-spring/renderprops";
+import VisibilitySensor from "react-visibility-sensor";
 
 class Human extends React.Component {
-	chartRef = React.createRef()
-	constructor(props) {
-		super()
-		this.state = {
-			data: props.data
-		}
-	}
-	render() {
-		return (
-			<section className="Human isWrapper">
-				<div className="Human-container isContainer">
-					<h2 className="ob-title isPrimary">{"mes valeurs"}</h2>
-					<ol className="List">
-						{this.state.data.map((humanSkill, HSIndex) => {
-							return (
-								<div
-									className="Prop cell small-auto medium-6 large-4 xlarge-3"
-									key={HSIndex}
-								>
-									<li className="Prop-name text-left">
-										{humanSkill.name}
-									</li>
-									<p className="Prop-quote text-left">
-										{humanSkill.quote}
-									</p>
-								</div>
-							)
-						})}
-					</ol>
-				</div>
-			</section>
-		)
-	}
+    chartRef = React.createRef();
+    constructor(props) {
+        super();
+        this.state = {
+            data: props.data
+        };
+    }
+    render() {
+        return (
+            <section className="Human isWrapper">
+                <div className="Human-container isContainer">
+                    <h2 className="ob-title isPrimary">{"mes valeurs"}</h2>
+                    <ol className="List">
+                        {this.state.data.map((humanSkill, HSIndex) => {
+                            return (
+                                <VisibilitySensor key={HSIndex}>
+                                    {({ isVisible }) => (
+                                        <Spring
+                                            config={{
+                                                duration: 300
+                                            }}
+                                            to={{
+                                                opacity: isVisible ? 1 : 0
+                                            }}
+                                        >
+                                            {props => (
+                                                <div
+                                                    className="Prop cell small-auto medium-6 large-4 xlarge-3"
+                                                    style={{ ...props }}
+                                                >
+                                                    <li className="Prop-name text-left">
+                                                        {humanSkill.name}
+                                                    </li>
+                                                    <p className="Prop-quote text-left">
+                                                        {humanSkill.quote}
+                                                    </p>
+                                                </div>
+                                            )}
+                                        </Spring>
+                                    )}
+                                </VisibilitySensor>
+                            );
+                        })}
+                    </ol>
+                </div>
+            </section>
+        );
+    }
 }
 
 Human.defaultProps = {
-	data: "<NONE>"
-}
+    data: "<NONE>"
+};
 
-export default Human
+export default Human;
--- src/pages/profile/Likes.js
+++ src/pages/profile/Likes.js
@@ -1,97 +1,94 @@
-import React from "react"
-import Loader from "react-loader-advanced"
-import { Spring } from "react-spring/renderprops"
-import VisibilitySensor from "react-visibility-sensor"
+import React from "react";
+import Loader from "react-loader-advanced";
+import { Spring } from "react-spring/renderprops";
+import VisibilitySensor from "react-visibility-sensor";
 
 /* COMPONENTS */
-import Spinner from "root/components/Spinner"
+import Spinner from "root/components/Spinner";
 
 class Likes extends React.Component {
-	constructor(props) {
-		super()
-		this.state = {
-			data: props.data,
-			showLoader: true
-		}
-		this._isMounted = false
-	}
-	componentDidMount() {
-		this._isMounted = true
-		const likeImg = new Image()
-		likeImg.src = this.state.data.jpg
+    constructor(props) {
+        super();
+        this.state = {
+            data: props.data,
+            showLoader: true
+        };
+        this._isMounted = false;
+    }
+    componentDidMount() {
+        this._isMounted = true;
+        const likeImg = new Image();
+        likeImg.src = this.state.data.jpg;
 
-		likeImg.onLoad = () => {
-			if (this._isMounted) {
-				this.setState({
-					showLoader: false
-				})
-			}
-		}
-	}
-	componentWillUnmount() {
-		this._isMounted = false
-	}
-	render() {
-		return (
-			<section className="Likes isWrapper">
-				<div className="Likes-container isContainer">
-					<h2 className="ob-title isPrimary">{"Ce que j'aime"}</h2>
-					<ul className="Likes-grid">
-						{this.state.data.map((taste, tasteIndex) => {
-							return (
-								<VisibilitySensor
-									partialVisibility
-									key={tasteIndex}
-								>
-									{({ isVisible }) => (
-										<Spring
-											config={{
-												duration: 300
-											}}
-											to={{
-												opacity: isVisible ? 1 : 0,
-												transform: isVisible
-													? "translateY(0)"
-													: "translateY(80px)"
-											}}
-										>
-											{props => (
-												<div
-													className="Like isEntry"
-													style={{ ...props }}
-												>
-													<div className="Like onCover">
-														<img
-															className="LikeImg"
-															src={taste.jpg}
-														/>
-														<div className="LikeSpan isEntry">
-															<li>
-																{taste.name}
-															</li>
-														</div>
-													</div>
-												</div>
-											)}
-										</Spring>
-									)}
-								</VisibilitySensor>
-							)
-						})}
-					</ul>
-				</div>
-			</section>
-		)
-	}
+        likeImg.onLoad = () => {
+            if (this._isMounted) {
+                this.setState({
+                    showLoader: false
+                });
+            }
+        };
+    }
+    componentWillUnmount() {
+        this._isMounted = false;
+    }
+    render() {
+        return (
+            <section className="Likes isWrapper">
+                <div className="Likes-container isContainer">
+                    <h2 className="ob-title isPrimary">{"Ce que j'aime"}</h2>
+                    <ul className="Likes-grid">
+                        {this.state.data.map((taste, tasteIndex) => {
+                            return (
+                                <VisibilitySensor
+                                    partialVisibility
+                                    key={tasteIndex}
+                                >
+                                    {({ isVisible }) => (
+                                        <Spring
+                                            config={{
+                                                duration: 300
+                                            }}
+                                            to={{
+                                                opacity: isVisible ? 1 : 0
+                                            }}
+                                        >
+                                            {props => (
+                                                <div
+                                                    className="Like isEntry"
+                                                    style={{ ...props }}
+                                                >
+                                                    <div className="Like onCover">
+                                                        <img
+                                                            className="LikeImg"
+                                                            src={taste.jpg}
+                                                        />
+                                                        <div className="LikeSpan isEntry">
+                                                            <li>
+                                                                {taste.name}
+                                                            </li>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            )}
+                                        </Spring>
+                                    )}
+                                </VisibilitySensor>
+                            );
+                        })}
+                    </ul>
+                </div>
+            </section>
+        );
+    }
 }
 
 Likes.defaultProps = {
-	data: [
-		{
-			name: "<NONE>",
-			jpg: "<JPG>"
-		}
-	]
-}
+    data: [
+        {
+            name: "<NONE>",
+            jpg: "<JPG>"
+        }
+    ]
+};
 
-export default Likes
+export default Likes;
